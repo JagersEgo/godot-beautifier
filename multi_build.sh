cdfb36ebb1ae31d2e0804d051d6848d5a06e4a72
JagersEgo <148036210+JagersEgo@users.noreply.github.com>
2025-05-18 20:49:39 +1000

    removed unused images, added build script

---
+++ multi_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm ./build/*
+
+GOOS=darwin GOARCH=arm64 go build -o build/godot-beautifier-mac-apple_sillicon
+GOOS=darwin GOARCH=amd64 go build -o build/godot-beautifier-mac-intel
+GOOS=windows GOARCH=amd64 go build -o build/godot-beautifier-win.exe
+GOOS=linux GOARCH=amd64 go build -o build/godot-beautifier-linux
