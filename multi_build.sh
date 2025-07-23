a547d072c295b0fab5b216c9e07e4b90c5468d6a
William Ashworth <z5689977@ad.unsw.edu.au>
2025-07-23 19:16:28 +1000

    remove broken build script

--- multi_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm ./build/*
-
-GOOS=darwin GOARCH=arm64 go build ./src -o build/godot-beautifier-mac-apple_sillicon
-GOOS=darwin GOARCH=amd64 go build ./src -o build/godot-beautifier-mac-intel
-GOOS=windows GOARCH=amd64 go build ./src -o build/godot-beautifier-win.exe
-GOOS=linux GOARCH=amd64 go build ./src -o build/godot-beautifier-linux
